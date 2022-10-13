20fa0c313eca12a990d46ca28336518be3af60a3
kbeachler <kaleigh8b@gmail.com>
2022-10-13 10:32:59 -0700

    reformat

--- SearchEngine.java
+++ SearchEngine.java
@@ -32,17 +32,17 @@ class Handler implements URLHandler {
             }
 
 
-class SearchEngine {
-    public static void main(String[] args) throws IOException {
-        if(args.length == 0){
-            System.out.println("Missing port number! Try any number between 1024 to 49151");
-            return;
+    class SearchEngine {
+        public static void main(String[] args) throws IOException {
+            if(args.length == 0){
+                System.out.println("Missing port number! Try any number between 1024 to 49151");
+                return;
+                }
+
+            int port = Integer.parseInt(args[0]);
+
+            Server.start(port, new Handler());
         }
-
-        int port = Integer.parseInt(args[0]);
-
-        Server.start(port, new Handler());
     }
-}
 
 }
